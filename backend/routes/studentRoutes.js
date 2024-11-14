9d9035e89ff6b9816295390996f3f675cd2b8952
Devin Shanesha <devin.shanesha@gmail.com>
2024-11-14 14:16:58 +0530

    backend changes

--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -1,12 +1,8 @@
 const express = require('express');
-const { getStudent, updateStudent, createStudentRegistration } = require('../controllers/studentController');
-const { protect, adminOnly } = require('../middleware/authMiddleware');
+const { createStudentRegistration } = require('../controllers/studentController');;
 const router = express.Router();
 
 // Routes for students
 router.post('/register', createStudentRegistration) // creating student in database
-router.get('/:id', protect, getStudent); // Both student and admin
-router.put('/:id', protect, updateStudent); // Both student and admin
-router.put('/admin/:id', protect, adminOnly, updateStudent); // Admin only
 
 module.exports = router;
--- backend/routes/testingRoute.js
+++
@@ -1,9 +0,0 @@
-// const express = require('express');
-// const router = express.Router();
-
-// router.post('/hello', (req, res)=>{
-//     const body = req.body;
-//     res.status(200).json({message: "hello"})
-// })
-
-// module.exports = router;
